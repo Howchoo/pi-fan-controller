10ee6afa368df9e2211926e0380d683b92dece40
Tyler <josephtylerjones@gmail.com>
2019-08-08 13:15:05 -0700

    Determine gpio and off limit with variables

--- fancontrol.py
+++ fancontrol.py
@@ -6,11 +6,23 @@ import time
 from gpiozero import OutputDevice
 
 
-LIMIT = 65  # degrees
-SLEEP_INTERVAL = 5  # seconds
+ON_THRESHOLD = 65  # (degrees Celsius) Fan kicks on at this temperature.
+OFF_THRESHOLD = 55  # (degress Celsius) Fan shuts off at this temperature.
+SLEEP_INTERVAL = 5  # (seconds) How often we check the core temperature.
+GPIO_PIN = 17  # Which GPIO pin you're using to control the fan.
 
 
 def get_temp():
+    """Get the core temperature.
+
+    Run a shell script to get the core temp and parse the output.
+
+    Raises:
+        RuntimeError: if response cannot be parsed.
+
+    Returns:
+        float: The core temperature in degrees Celsius.
+    """
     output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
     temp_str = output.stdout.decode()
     try:
@@ -20,21 +32,24 @@ def get_temp():
 
 
 if __name__ == '__main__':
-    fan = OutputDevice(17)
+    # Validate the on and off thresholds
+    if OFF_THRESHOLD >= ON_THRESHOLD:
+        raise RuntimeError('OFF_THRESHOLD must be less than ON_THRESHOLD')
+
+    fan = OutputDevice(GPIO_PIN)
 
     while True:
         temp = get_temp()
 
-
         # Start the fan if the temperature has reached the limit and the fan
         # isn't already running.
         # NOTE: `fan.value` returns 1 for "on" and 0 for "off"
-        if temp > LIMIT and not fan.value:
+        if temp > ON_THRESHOLD and not fan.value:
             fan.on()
 
         # Stop the fan if the fan is running and the temperature has dropped
         # to 10 degrees below the limit.
-        elif fan.value and temp < (LIMIT - 10):
+        elif fan.value and temp < OFF_THRESHOLD:
             fan.off()
 
         time.sleep(SLEEP_INTERVAL)
