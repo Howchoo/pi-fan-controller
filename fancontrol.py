c76218b2e6142b29225deb5818dcfb81dc9c687d
Tobias Daeullary <t-dae@live.de>
2023-04-25 03:35:59 +0100

    Add initial PWM

--- fancontrol.py
+++ fancontrol.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 import time
+import logging
 
-from gpiozero import OutputDevice
+from gpiozero import PWMOutputDevice
 
 
-ON_THRESHOLD = 65  # (degrees Celsius) Fan kicks on at this temperature.
-OFF_THRESHOLD = 55  # (degress Celsius) Fan shuts off at this temperature.
-SLEEP_INTERVAL = 5  # (seconds) How often we check the core temperature.
-GPIO_PIN = 17  # Which GPIO pin you're using to control the fan.
+FULL_ON_TEMP = 65.  # [degC] temp for max PWM
+OFF_TEMP = 55.  # [degC] temp for min PWM
+INTERVAL = 5.  # [s] refresh interval
+GPIO_PIN = 17  # Pin to connect transistor base
+MAX_PWM = 1.  # PWM at max on temp
+MIN_PWM = .7  # PWM at lowest on temp
+MIN_STARTUP_PWM = .9  # PWM to use on startup
+PWM_FREQUENCY = 500  # [Hz] - with 100uF, and .7 min PWM
 
 
 def get_temp():
@@ -27,25 +32,34 @@ def get_temp():
     except (IndexError, ValueError,) as e:
         raise RuntimeError('Could not parse temperature output.') from e
 
-if __name__ == '__main__':
-    # Validate the on and off thresholds
-    if OFF_THRESHOLD >= ON_THRESHOLD:
-        raise RuntimeError('OFF_THRESHOLD must be less than ON_THRESHOLD')
 
-    fan = OutputDevice(GPIO_PIN)
+def turn_on_or_off(pwm_device: PWMOutputDevice, temp: int):
+    if fan.is_active and temp < OFF_TEMP:
+        fan.off()
+        print("Turned off")
+    elif not fan.is_active and temp >= OFF_TEMP:
+        fan.on()
+        fan.value = MIN_STARTUP_PWM
+        print("Turned on")
+
+
+if __name__ == "__main__":
+    assert OFF_TEMP < FULL_ON_TEMP, "OFF_TEMP must be lower than FULL_ON_TEMP"
+    assert MIN_STARTUP_PWM >= MIN_PWM, "MIN_STARTUP_PWM must be at least MIN_PWM"
+    assert MAX_PWM > MIN_PWM, "MAX_PWM must be higher than MIN_PWM"
+
+    fan = PWMOutputDevice(GPIO_PIN, frequency=PWM_FREQUENCY)
+    turn_on_or_off(fan, get_temp())
+
+    pwm_range = MAX_PWM - MIN_PWM
+    temp_range = FULL_ON_TEMP - OFF_TEMP
 
     while True:
         temp = get_temp()
+        pwm = pwm_range * (temp - OFF_TEMP) / temp_range + MIN_PWM
+        if fan.is_active:
+            fan.value = max(min(pwm, MAX_PWM), MIN_PWM)
+        turn_on_or_off(fan, temp)
 
-        # Start the fan if the temperature has reached the limit and the fan
-        # isn't already running.
-        # NOTE: `fan.value` returns 1 for "on" and 0 for "off"
-        if temp > ON_THRESHOLD and not fan.value:
-            fan.on()
-
-        # Stop the fan if the fan is running and the temperature has dropped
-        # to 10 degrees below the limit.
-        elif fan.value and temp < OFF_THRESHOLD:
-            fan.off()
-
-        time.sleep(SLEEP_INTERVAL)
+        print(f"Current temp|PWM: {temp:.2f}|{fan.value:.2f}")
+        time.sleep(INTERVAL)
